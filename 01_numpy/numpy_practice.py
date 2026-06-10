1a198b855ea911ba274bfc4565c893ea0e55a7ba
Chanwoo1124 <lgw22774@gmail.com>
2026-06-10 13:21:09 +0900

    numpy_practice.py: N/D 변수 추가 및 가중치·데이터 생성 코드 수정

--- 01_numpy/numpy_practice.py
+++ 01_numpy/numpy_practice.py
@@ -1,15 +1,18 @@
 import numpy as np
 
+N, D = 5, 4
 
-w_true = np.array([0.2,0.4,0.6,0.8]).reshape(-1,1)
+w_true = np.array([0.2,0.4,0.5,0.7]).reshape(-1,1)
 
 b_true = 2.0
 
-x_data = (np.random.random((5, 4)) - 0.5 ) * 8
 
-y_data = x_data @ w_true + b_true
+y_data = np.random.random((N, D) - 0,5) * 8
+
+y_pred = x_data @ w_true + b_true # N, 1
 
 print(y_data)
+
 
 
 
